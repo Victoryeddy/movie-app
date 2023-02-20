eee09c1fbda81389fc89f9504c56fb62fb438547
Victory Ndu <victor5ndu@gmail.com>
2023-02-20 12:56:29 +0100

    add vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<p>hello world</p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+/* eslint-disable */
+  name: 'Home',
 }
 </script>
